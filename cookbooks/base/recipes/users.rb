c935ec8ae7d0c24df7a28cc9a56e076cc3ee2160
Colby Olson <me@colbyolson.com>
2020-04-02 22:55:33 -0700

    GROUPS!

--- cookbooks/base/recipes/users.rb
+++ cookbooks/base/recipes/users.rb
@@ -9,18 +9,15 @@ user 'colby' do
   manage_home true
 end
 
-group 'adm' do
-  action           :modify
-  members          'colby'
-  append           true
-  excluded_members 'ubuntu'
-end
+groups = %w(adm sudo admin)
 
-group 'sudo' do
-  action           :modify
-  members          'colby'
-  append           true
-  excluded_members 'ubuntu'
+groups.each do |g|
+  group g do
+    action           :modify
+    members          'colby'
+    append           true
+    excluded_members 'ubuntu'
+  end
 end
 
 directory '/home/colby/.ssh' do
@@ -34,4 +31,5 @@ file '/home/colby/.ssh/authorized_keys' do
   content 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJPXyRqEc8u4+UGDhCMKoEYbH0cUEI/04ipBmfJlNDhXN56wgWAAZQQ3qeNUSOGPwPUfTSX6JZNM2qr0KVVLwxNLDcRq0AXQWSZKgdJpSsX161lwO0Ypmm21lyOuU5uHAUP4ju1+TzvQhFask9C8yeVfGVsMHrbQ4IBaPievJ5OjX/Wv9r/eMmGLFGCYZaU6rtGo9hQu/ZJVYPwmFXF1RS6Sg+GmHt6lYcB2mJjVQN92HIeJwD+Mtudpupq/5sWr1X9tb7w3jelUwAyIb7JaWV/mmvhnUyi5hxiOkQGoDFXqEoHXInlOmOay56xOzfpskRDAuTYDt+CkpxKqNhr17x colbyo@copio.us'
   mode    0600
   owner   'colby'
+  group   'colby'
 end

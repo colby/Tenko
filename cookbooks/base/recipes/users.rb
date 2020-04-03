e1d29b3a518c85e28a402026dd8a4daad71060f0
Colby Olson <me@colbyolson.com>
2020-04-02 22:49:56 -0700

    remove ubuntu from groups

--- cookbooks/base/recipes/users.rb
+++ cookbooks/base/recipes/users.rb
@@ -10,20 +10,28 @@ user 'colby' do
 end
 
 group 'adm' do
-  action :modify
-  members 'colby'
-  append true
+  action           :modify
+  members          'colby'
+  append           true
+  excluded_members 'ubuntu'
+end
+
+group 'sudo' do
+  action           :modify
+  members          'colby'
+  append           true
+  excluded_members 'ubuntu'
 end
 
 directory '/home/colby/.ssh' do
-  mode 0755
+  mode  0755
   owner 'colby'
   group 'colby'
 end
 
 file '/home/colby/.ssh/authorized_keys' do
-  action :create
+  action  :create
   content 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJPXyRqEc8u4+UGDhCMKoEYbH0cUEI/04ipBmfJlNDhXN56wgWAAZQQ3qeNUSOGPwPUfTSX6JZNM2qr0KVVLwxNLDcRq0AXQWSZKgdJpSsX161lwO0Ypmm21lyOuU5uHAUP4ju1+TzvQhFask9C8yeVfGVsMHrbQ4IBaPievJ5OjX/Wv9r/eMmGLFGCYZaU6rtGo9hQu/ZJVYPwmFXF1RS6Sg+GmHt6lYcB2mJjVQN92HIeJwD+Mtudpupq/5sWr1X9tb7w3jelUwAyIb7JaWV/mmvhnUyi5hxiOkQGoDFXqEoHXInlOmOay56xOzfpskRDAuTYDt+CkpxKqNhr17x colbyo@copio.us'
-  mode 0600
-  owner 'colby'
+  mode    0600
+  owner   'colby'
 end

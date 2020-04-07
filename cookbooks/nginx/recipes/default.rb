a01303b232618460b7b67b0434b7a89eb1ce3d9b
Colby Olson <colson2@wsgc.com>
2020-04-07 15:09:02 -0700

    ubuntu flavor

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -5,6 +5,11 @@
 
 package node['nginx']['packages']
 
+case node['platform']
+when 'ubuntu'
+  include_recipe 'nginx::ubuntu'
+end
+
 service 'nginx' do
   action [:enable, :start]
 end
--- cookbooks/nginx/recipes/ubuntu.rb
+++ cookbooks/nginx/recipes/ubuntu.rb
@@ -3,7 +3,7 @@
 # Recipe:: ubuntu
 #
 
-template '/etc/nginx/text.conf' do
+template '/etc/nginx/nginx.conf' do
   source   'nginx.conf.erb'
   notifies :reload, 'service[nginx]', :delayed
 end

d74f53762840fd7ef4ef4e613b3bcbd379991c5e
Colby Olson <colson2@wsgc.com>
2020-04-08 22:51:52 -0700

    wrong order

--- cookbooks/tailscale/recipes/default.rb
+++ cookbooks/tailscale/recipes/default.rb
@@ -3,10 +3,10 @@
 # Recipe:: default
 #
 
-package node['tailscale']['packages']
-
 apt_repository 'tailscale' do
   uri 'https://pkgs.tailscale.com/stable/ubuntu'
   components ['main']
   key 'https://pkgs.tailscale.com/stable/ubuntu/bionic.gpg'
 end
+
+package node['tailscale']['packages']

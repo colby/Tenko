ab6e30b6a37a62ac221da3e80436afc724fc1b1e
Colby Olson <colson2@wsgc.com>
2020-04-08 23:11:39 -0700

    another dumb attempt

--- cookbooks/tailscale/recipes/default.rb
+++ cookbooks/tailscale/recipes/default.rb
@@ -6,7 +6,8 @@
 apt_repository 'tailscale' do
   uri 'https://pkgs.tailscale.com/stable/ubuntu'
   components ['main']
-  key ['https://pkgs.tailscale.com/stable/ubuntu/bionic.gpg','bionic.gpg']
+  keyserver 'https://pkgs.tailscale.com/stable/ubuntu/bionic.gpg'
+  key 'bionic.gpg'
 end
 
 package node['tailscale']['packages']

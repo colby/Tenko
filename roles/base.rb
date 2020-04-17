46a6eaa0ae4ccf03854f9854baca5395e71335ce
Colby Olson <me@colbyolson.com>
2020-04-16 20:29:04 -0700

    add http(s) to iptables

--- roles/base.rb
+++ roles/base.rb
@@ -6,6 +6,8 @@ default_attributes(
   iptables: {
     rules: [
       '-A INPUT -i eth0 -p tcp -m tcp --dport 22 -j ACCEPT',
+      '-A INPUT -i eth0 -p tcp -m tcp --dport 80 -j ACCEPT',
+      '-A INPUT -i eth0 -p tcp -m tcp --dport 443 -j ACCEPT',
     ]
   }
 )

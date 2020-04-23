44baa8e3ffe6b28374ef34799043b0987b991659
Colby Olson <colson2@wsgc.com>
2020-04-23 16:24:22 -0700

    use netrc to save password

--- cookbooks/sendmail/recipes/default.rb
+++ cookbooks/sendmail/recipes/default.rb
@@ -8,3 +8,8 @@ package node['sendmail']['packages']
 template '/etc/msmtprc' do
   source 'msmtprc.erb'
 end
+
+template '/home/colby/.netrc' do
+  source 'netrc.erb'
+  action :create_if_missing # NOTE: local edits will be preserved
+end

a8742754cbcadb36ca986e1d48b0d380f8eb6cb7
Colby Olson <colson2@wsgc.com>
2020-04-23 14:53:53 -0700

    harden the service

--- cookbooks/fail2ban/recipes/default.rb
+++ cookbooks/fail2ban/recipes/default.rb
@@ -5,10 +5,22 @@
 
 package node['fail2ban']['packages']
 
-# template '' do
-#   source   ''
-#   notifies :reload, 'service[]', :immediately
-# end
+directory '/var/log/fail2ban/'
+
+template '/etc/systemd/system/fail2ban.service.d/override.conf' do
+  source 'override.conf.erb'
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+end
+
+template '/etc/fail2ban/fail2ban.local' do
+  source 'fail2ban.local.erb'
+  notifies :restart, 'service[fail2ban]', :delayed
+end
+
+template '/etc/fail2ban/jail.local' do
+  source 'jail.local.erb'
+  notifies :restart, 'service[fail2ban]', :delayed
+end
 
 service 'fail2ban' do
   action [:enable, :start]

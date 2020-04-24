905cd149393451573e3701349cd46b227805617c
Colby Olson <colson2@wsgc.com>
2020-04-23 17:03:11 -0700

    netrc was a dumb idea

--- cookbooks/sendmail/recipes/default.rb
+++ cookbooks/sendmail/recipes/default.rb
@@ -7,9 +7,5 @@ package node['sendmail']['packages']
 
 template '/etc/msmtprc' do
   source 'msmtprc.erb'
-end
-
-template '/home/colby/.netrc' do
-  source 'netrc.erb'
   action :create_if_missing # NOTE: local edits will be preserved
 end
